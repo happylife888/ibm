f0c0fe94125706334b4d416688317dd39f7da6ea
happylife888 <71301030+happylife888@users.noreply.github.com>
2020-09-27 17:25:56 +0800

    Update a_ins.sh

--- a_ins.sh
+++ a_ins.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+wget --no-check-certificate -O index.php https://raw.githubusercontent.com/happylife888/ibm/master/index.php
 wget --no-check-certificate -O a_com.sh https://raw.githubusercontent.com/happylife888/ibm/master/a_com.sh
 chmod +x ./a_com.sh
 wget --no-check-certificate -O manifest.yml https://raw.githubusercontent.com/happylife888/ibm/master/manifest.yml
 ibmcloud target --cf
 ibmcloud cf push
+echo 'lat'
