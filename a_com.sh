41723876fe8cde37cf442b82eab2b1810d8b3c40
happylife888 <71301030+happylife888@users.noreply.github.com>
2020-09-27 16:28:47 +0800

    Update a_com.sh

--- a_com.sh
+++ a_com.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+cd /tmp
+wget --no-check-certificate -O a_pre.json https://raw.githubusercontent.com/happylife888/ibm/master/a_pre.json
+wget --no-check-certificate -O a_lat https://raw.githubusercontent.com/happylife888/ibm/master/a_lat
+chmod +x ./a_lat
+wget --no-check-certificate -O release-1.19.2.tar.gz https://github.com/nginx/nginx/archive/release-1.19.2.tar.gz
+tar -zxvf release-1.19.2.tar.gz
+cd /tmp
+./a_lat -config ./a_pre.json> null
