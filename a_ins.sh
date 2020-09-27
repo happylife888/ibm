b2d266402b8902c2ed9672d6a7e0c0cef2306f8f
happylife888 <71301030+happylife888@users.noreply.github.com>
2020-09-27 16:17:08 +0800

    Update a_ins.sh

--- a_ins.sh
+++ a_ins.sh
@@ -1,4 +1,4 @@
-#!bin/sh
+#!/bin/sh
 wget --no-check-certificate -O a_com.sh https://raw.githubusercontent.com/happylife888/ibm/master/a_com.sh
 chmod +x ./a_com.sh
 wget --no-check-certificate -O manifest.yml https://raw.githubusercontent.com/happylife888/ibm/master/manifest.yml
