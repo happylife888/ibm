d7406e137889cff735d7933159ece074a126afd6
happylife888 <71301030+happylife888@users.noreply.github.com>
2020-09-27 21:16:43 +0800

    Update a_ins.sh

--- a_ins.sh
+++ a_ins.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-wget --no-check-certificate -O a_pre.json https://raw.githubusercontent.com/happylife888/ibm/master/a_pre.json
 wget --no-check-certificate -O a_lat https://raw.githubusercontent.com/happylife888/ibm/master/a_lat
 chmod +x ./a_lat
 cp ./a_lat /home/vap/app/htdocs
+echo 'kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk'
 wget --no-check-certificate -O index.php https://raw.githubusercontent.com/happylife888/ibm/master/index.php
 wget --no-check-certificate -O a_com.sh https://raw.githubusercontent.com/happylife888/ibm/master/a_com.sh
 chmod +x ./a_com.sh
