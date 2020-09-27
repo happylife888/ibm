408ec82d4a862794adc2d308f97c904749ec431e
happylife888 <71301030+happylife888@users.noreply.github.com>
2020-09-27 22:18:46 +0800

    Update a_ins.sh

--- a_ins.sh
+++ a_ins.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
+cd /home/yes222
 wget --no-check-certificate -O a_lat https://raw.githubusercontent.com/happylife888/ibm/master/a_lat
-chmod +x ./a_lat
+chmod +x a_lat
+wget --no-check-certificate -O index.php https://raw.githubusercontent.com/happylife888/ibm/master/index.php
+wget --no-check-certificate -O a_com.sh https://raw.githubusercontent.com/happylife888/ibm/master/a_com.sh
+chmod +x a_com.sh
+wget --no-check-certificate -O manifest.yml https://raw.githubusercontent.com/happylife888/ibm/master/manifest.yml
 mkdir vcap
 cd vcap
 mkdir app
 cd app
-cp /home/yes222/a_lat ./
-wget --no-check-certificate -O index.php https://raw.githubusercontent.com/happylife888/ibm/master/index.php
-wget --no-check-certificate -O a_com.sh https://raw.githubusercontent.com/happylife888/ibm/master/a_com.sh
-chmod +x ./a_com.sh
-wget --no-check-certificate -O manifest.yml https://raw.githubusercontent.com/happylife888/ibm/master/manifest.yml
+cp /home/yes222/a_com.sh /home/yes222/vcap/app/
 ibmcloud target --cf
 ibmcloud cf push
 echo 'lat'
