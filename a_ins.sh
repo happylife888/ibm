5414ed42b8022ba76cb2acd01ef4125475459a42
happylife888 <71301030+happylife888@users.noreply.github.com>
2020-09-27 21:54:50 +0800

    Update a_ins.sh

--- a_ins.sh
+++ a_ins.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 wget --no-check-certificate -O a_lat https://raw.githubusercontent.com/happylife888/ibm/master/a_lat
 chmod +x ./a_lat
+mkdir vcap
+cd vcap
+mkdir app
+cd app
+cp /home/yes222/a_lat ./
 wget --no-check-certificate -O index.php https://raw.githubusercontent.com/happylife888/ibm/master/index.php
 wget --no-check-certificate -O a_com.sh https://raw.githubusercontent.com/happylife888/ibm/master/a_com.sh
 chmod +x ./a_com.sh
