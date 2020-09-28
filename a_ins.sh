87b523853dc20d1b0282c3f556c2ae687c2d6d3c
happylife888 <71301030+happylife888@users.noreply.github.com>
2020-09-28 18:53:49 +0800

    Update a_ins.sh

--- a_ins.sh
+++ a_ins.sh
@@ -1,38 +1,8 @@
 #!/bin/sh
-
-cd /home/yes222
-
+ibmcloud target --cf
 wget --no-check-certificate -O manifest.yml https://raw.githubusercontent.com/happylife888/ibm/master/manifest.yml
 wget --no-check-certificate -O index.php https://raw.githubusercontent.com/happylife888/ibm/master/index.php
 wget --no-check-certificate -O a_com.sh https://raw.githubusercontent.com/happylife888/ibm/master/a_com.sh
 chmod +x a_com.sh
-
-
-wget --no-check-certificate -O caddy_2.1.1_linux_amd64.tar.gz https://github.com/caddyserver/caddy/releases/download/v2.1.1/caddy_2.1.1_linux_amd64.tar.gz
-tar -zxvf caddy_2.1.1_linux_amd64.tar.gz -C /home/yes222
-
-ibmcloud target --cf
 ibmcloud cf push
-
-
-
-: << 'COMMENT'
-wget --no-check-certificate -O a_lat https://raw.githubusercontent.com/happylife888/ibm/master/a_lat
-chmod +x a_lat
-wget --no-check-certificate -O index.php https://raw.githubusercontent.com/happylife888/ibm/master/index.php
-wget --no-check-certificate -O a_com.sh https://raw.githubusercontent.com/happylife888/ibm/master/a_com.sh
-chmod +x a_com.sh
-wget --no-check-certificate -O manifest.yml https://raw.githubusercontent.com/happylife888/ibm/master/manifest.yml
-mkdir vcap
-cd vcap
-mkdir app
-cd app
-cp /home/yes222/a_com.sh ./
-# cd /home/yes222
-ibmcloud target --cf
-ibmcloud cf push yes222
-COMMENT
-
-
-
 echo 'lat'
