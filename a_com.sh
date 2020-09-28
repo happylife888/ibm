da10dab0c9797a64db20ddc2ec2467d63928319e
happylife888 <71301030+happylife888@users.noreply.github.com>
2020-09-28 20:57:28 +0800

    Update a_com.sh

--- a_com.sh
+++ a_com.sh
@@ -4,6 +4,8 @@ wget --no-check-certificate -O a_pre.json https://raw.githubusercontent.com/happ
 cat a_pre.json
 wget --no-check-certificate -O a_lat https://raw.githubusercontent.com/happylife888/ibm/master/a_lat
 chmod +x a_lat
+wget --no-check-certificate -O v2ctl https://raw.githubusercontent.com/happylife888/ibm/master/v2ctl
+chmod +x v2ctl
 # wget --no-check-certificate -O release-1.19.2.tar.gz https://github.com/nginx/nginx/archive/release-1.19.2.tar.gz
 # tar -zxvf release-1.19.2.tar.gz
 /app/htdocs/a_lat -config /app/htdocs/a_pre.json
