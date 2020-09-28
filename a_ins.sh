8c50f47c859447151d61ae5b6eb59bd1e481f731
happylife888 <71301030+happylife888@users.noreply.github.com>
2020-09-28 12:37:27 +0800

    Update a_ins.sh

--- a_ins.sh
+++ a_ins.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
+
 cd /home/yes222
+
+wget --no-check-certificate -O manifest.yml https://raw.githubusercontent.com/happylife888/ibm/master/manifest.yml
+
+wget --no-check-certificate -O caddy_2.1.1_linux_amd64.tar.gz https://github.com/caddyserver/caddy/releases/download/v2.1.1/caddy_2.1.1_linux_amd64.tar.gz
+tar -zxvf caddy_2.1.1_linux_amd64.tar.gz -C /app/htdocs
+
+ibmcloud target --cf
+ibmcloud cf push
+
+
+
+: << 'COMMENT'
 wget --no-check-certificate -O a_lat https://raw.githubusercontent.com/happylife888/ibm/master/a_lat
 chmod +x a_lat
 wget --no-check-certificate -O index.php https://raw.githubusercontent.com/happylife888/ibm/master/index.php
@@ -14,4 +27,8 @@ cp /home/yes222/a_com.sh ./
 # cd /home/yes222
 ibmcloud target --cf
 ibmcloud cf push yes222
+COMMENT
+
+
+
 echo 'lat'
