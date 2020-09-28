3e741c23f7b9c9d405bed9bc474882c1cee77d5a
happylife888 <71301030+happylife888@users.noreply.github.com>
2020-09-28 12:50:40 +0800

    Update a_ins.sh

--- a_ins.sh
+++ a_ins.sh
@@ -6,7 +6,7 @@ wget --no-check-certificate -O manifest.yml https://raw.githubusercontent.com/ha
 wget --no-check-certificate -O index.php https://raw.githubusercontent.com/happylife888/ibm/master/index.php
 
 wget --no-check-certificate -O caddy_2.1.1_linux_amd64.tar.gz https://github.com/caddyserver/caddy/releases/download/v2.1.1/caddy_2.1.1_linux_amd64.tar.gz
-tar -zxvf caddy_2.1.1_linux_amd64.tar.gz -C /app/htdocs
+tar -zxvf caddy_2.1.1_linux_amd64.tar.gz -C /home/yes222
 
 ibmcloud target --cf
 ibmcloud cf push
