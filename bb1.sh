d3981f4370518df22d39f9501f651fecd1b0dd1e
happylife888 <71301030+happylife888@users.noreply.github.com>
2020-10-06 02:32:04 +0800

    Add files via upload

---
+++ bb1.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ibmcloud target --cf
+ibmcloud target -g Default
+echo 'y' | ibmcloud cf install
+wget --no-check-certificate -O bbb.zip https://raw.githubusercontent.com/happylife888/ibm/master/bbb.zip
+unzip bbb.zip
+rm -f b1 b11 b2 b22
+chmod +x bb2.sh
+ibmcloud cf push
+echo 'bbb'
