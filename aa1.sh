7a8a65052c627b19cdf69ab8c855e55cf71ed831
happylife888 <71301030+happylife888@users.noreply.github.com>
2020-10-06 02:30:12 +0800

    Delete aa1.sh

--- aa1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ibmcloud target --cf
-ibmcloud target -g Default
-wget --no-check-certificate -O aaa.zip https://raw.githubusercontent.com/happylife888/ibm/master/aaa.zip
-unzip aaa.zip
-rm -f aaa.zip
-chmod +x aa2.sh a1 a2
-ibmcloud cf push
-echo 'aaa'
